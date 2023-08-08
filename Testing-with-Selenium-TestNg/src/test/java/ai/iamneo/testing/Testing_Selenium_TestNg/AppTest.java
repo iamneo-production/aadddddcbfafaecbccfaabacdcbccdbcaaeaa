f472f33fc7d6bf52e286e628d1c02666cce1fedd
Iamneo <techadmin@iamneo.ai>
2023-08-08 10:16:53 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest {
 	@BeforeTest
 	public void beforeTest() throws Exception {
 	//	System.setProperty("webdriver.chrome.driver", "/home/coder/project/workspace/Testing-with-Selenium-TestNg/chromedriver");
-		driver = naew RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
 	}
 
 	public void testcase_1() throws InterruptedException {
@@ -32,17 +32,32 @@ public class AppTest {
 	public void TestCase_2() throws InterruptedException {
 		String title= "Droppable | jQuery UI";
 		 WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.titleContains(title));
-driver.switchTo().frame(0);
-        WebElement source = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#draggable"))));
-        WebElement target = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#droppable"))));
+		 wait.until(ExpectedConditions.titleContains(title));
+		driver.switchTo().frame(0);
+		 WebElement source = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#draggable"))));
+		WebElement target = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#droppable"))));
 
-        Actions actions = new Actions(driver);
-        actions.dragAndDrop(source, target).build().perform();
-
-        String getText = target.getText();
-        Assert.assertEquals(getText, "Dropped!");
+		 Actions actions = new Actions(driver);
+		actions.dragAndDrop(source, target).build().perform();
+		String getText = target.getText();
+		 Assert.assertEquals(getText, "Dropped!");
     }
+	
+	@Test
+	public void testcase_3() throws InterruptedException{
+
+		driver.get("http://jqueryui.com/droppable/"); 
+ 		String title = "Droppable | jQuery UI";
+		Assert.assertEquals(driver.getTitle(), title);
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		wait.until(ExpectedConditions.titleContains(title));
+		driver.switchTo().frame(0);
+		WebElement target = driver.findElement(By.cssSelector("#droppable"));
+		String color = target.getCssValue("background-color");
+		Assert.assertEquals(color, "rgba(233, 233, 233, 1)");
+
 	}
 
 	@AfterTest
