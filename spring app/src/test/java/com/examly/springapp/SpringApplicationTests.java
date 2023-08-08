fc085f9f66ce4537dd7d9fc38c327b6747367e89
Iamneo <techadmin@iamneo.ai>
2023-08-08 10:46:30 +0000

    Backup Commit

--- spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.Assert;
